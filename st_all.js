5f7262220a51f51c108e48a582ab26affb0881ef
Ishikawa1007 <kota.stone08.20@gmail.com>
2026-01-15 22:24:41 +0900

    Update Mediapipe to v0.10.26 (latest stable)

--- st_all.js
+++ st_all.js
@@ -707,7 +707,7 @@ function startMediapipeHands(){
   }
   if (video.readyState >= 2) { console.log('video ready immediately'); setCanvasSizeToVideo(); }
   else { console.log('video not ready, waiting for loadedmetadata'); video.addEventListener('loadedmetadata', setCanvasSizeToVideo, {once:true}); }
-  const MP_VERSION = "0.4.164";
+  const MP_VERSION = "0.10.26";
 
   mpHands = new Hands({
   locateFile: (file) =>
